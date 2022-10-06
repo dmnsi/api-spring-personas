41c4a70c06043764a4826c8786a10f55c097602d
Damian <dmnsimon@gmail.com>
2022-10-06 16:22:36 -0300

    guardarUsuario

--- src/main/java/com/api/controllers/PersonaController.java
+++ src/main/java/com/api/controllers/PersonaController.java
@@ -2,13 +2,10 @@ package com.api.controllers;
 
 import com.api.model.PersonaModel;
 import com.api.services.PersonaService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -22,7 +19,7 @@ public class PersonaController {
         return personaService.obtenerPersonas();
     }
     
-    @PostMapping()
+    @PostMapping("/")
     public PersonaModel guardarUsuario(@RequestBody PersonaModel persona){
         return this.personaService.guardarPersona(persona);
     }
